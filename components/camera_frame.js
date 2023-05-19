e6fca80ef57a61b3963bb3e0dd4aa0dff3224c2e
Lilian Giselly <lgps@ic.ufal.br>
2023-05-19 16:40:50 -0300

    revert changes

--- components/camera_frame.js
+++ components/camera_frame.js
@@ -11,7 +11,7 @@ class CameraFrame {
     draw() {
         this.videoElement = document.createElement('video');
         this.videoElement.className = 'input_video';
-        // this.videoElement.style = 'display: none';
+        this.videoElement.style = 'display: none';
 
         this.outputCanvas = document.createElement('canvas');
         this.outputCanvas.className = 'output_canvas';
