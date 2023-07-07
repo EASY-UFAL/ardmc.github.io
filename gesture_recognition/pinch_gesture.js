75bc5a3e2d37ae80b678226a592af3389e3357a7
Lilian Giselly <lgps@ic.ufal.br>
2023-07-07 18:10:40 -0300

    reverting all of the last changes

--- gesture_recognition/pinch_gesture.js
+++ gesture_recognition/pinch_gesture.js
@@ -62,8 +62,8 @@ class PinchGesture {
             let coords1 = HAND[0][fingerIndices[0]];
             let coords2 = HAND[0][fingerIndices[1]];
 
-            let x = landmarks.x;
-            let y = landmarks.y;
+            let x = landmarks.x * this.outputCanvas.width;
+            let y = landmarks.y * this.outputCanvas.height;
             let z = Math.abs(landmarks.z);
             let markColor = "black";
 
@@ -92,7 +92,7 @@ class PinchGesture {
               canvasCtx.arc(
                 (X1 + X2) / 2,
                 (Y1 + Y2) / 2,
-                (Z1 + Z2) / 2,
+                (10 * (Z1 + Z2)) / 2,
                 0,
                 2 * Math.PI
               );
@@ -139,7 +139,7 @@ class PinchGesture {
             }
 
             canvasCtx.beginPath();
-            canvasCtx.arc(x, y, z, 0, 2 * Math.PI);
+            canvasCtx.arc(x, y, 10 * z, 0, 2 * Math.PI);
             canvasCtx.fillStyle = markColor;
             canvasCtx.fill();
             canvasCtx.stroke();
